13228f2a7b723c5ad0d6af5836e90bdf363b54da
Matthew Shin <mshin1995@gmail.com>
2019-02-13 06:57:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,12 +22,26 @@ def shift_with_args(array)
   
 end
 
-def using_concat
-  
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
 def using_insert
   
 end
 
-using_
+def using_uniq
+  
+end
+
+def using_flatten
+
+end
+
+def using_delete
+
+end
+
+def using_delete_at
+  
+end
